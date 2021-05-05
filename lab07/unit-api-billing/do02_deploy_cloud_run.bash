91202cc9ee28bf259bec0151c6ebb9e0b4f4e5e9
hnishi <nishigami2509@gmail.com>
2021-05-05 23:49:58 +0000

    fix task1

--- lab07/unit-api-billing/do02_deploy_cloud_run.bash
+++ lab07/unit-api-billing/do02_deploy_cloud_run.bash
@@ -2,5 +2,5 @@ gcloud run deploy public-billing-service \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/billing-staging-api:0.1 \
   --platform managed \
   --region us-central1 \
-  --no-allow-unauthenticated
+  --allow-unauthenticated
 
