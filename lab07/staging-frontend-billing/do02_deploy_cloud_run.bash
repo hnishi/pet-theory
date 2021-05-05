2d65bf6529bac4cc2ec9c6f9a01ce58825b85a0d
hnishi <nishigami2509@gmail.com>
2021-05-05 23:52:55 +0000

    fix task2

--- lab07/staging-frontend-billing/do02_deploy_cloud_run.bash
+++ lab07/staging-frontend-billing/do02_deploy_cloud_run.bash
@@ -2,4 +2,4 @@ gcloud run deploy frontend-staging-service \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/frontend-staging:0.1 \
   --platform managed \
   --region us-central1 \
-  --no-allow-unauthenticated
+  --allow-unauthenticated
--- lab07/staging-frontend-billing/do03_test_service.bash
+++ lab07/staging-frontend-billing/do03_test_service.bash
@@ -2,5 +2,5 @@ SERVICE_URL=$(gcloud run services describe frontend-staging-service \
   --platform managed \
   --region us-central1 \
   --format "value(status.url)")
-echo $SERVICE_URL
 curl -X GET $SERVICE_URL
+echo see the web page in a browser: $SERVICE_URL
