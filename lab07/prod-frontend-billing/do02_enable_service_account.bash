424dd8ddeefb711d3233772fd4a3e35442a562cd
hnishi <nishigami2509@gmail.com>
2021-05-06 00:03:57 +0000

    fix task6

--- lab07/prod-frontend-billing/do02_enable_service_account.bash
+++ lab07/prod-frontend-billing/do02_enable_service_account.bash
@@ -1,5 +1,5 @@
 gcloud run services add-iam-policy-binding frontend-prod-service \
-  --member=serviceAccount:billing-service-sa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
+  --member=serviceAccount:frontend-service-sa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
   --role=roles/run.invoker \
   --region us-central1 \
   --platform managed
